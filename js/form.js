b66f8339415cfa0049c987edb1d9f6a676de9d74
Lucas Viana <lucas.lvtorres@gmail.com>
2021-10-09 16:50:49 -0400

    Corregindo -- JESUS CRISTO É O SENHOR!

--- js/form.js
+++ js/form.js
@@ -1,37 +1,16 @@
 var botao = document.querySelector("#adicionar-paciente") //documento é uma copia do html, meu DOM
+
 botao.addEventListener("click", function() {
+
     event.preventDefault() //não executa o eventos padroes
     var form = document.querySelector("#form")
     console.log("botao clicado, GRAÇAS A DEUS!!")
 
     var paciente = obtendoDadosDoForm(form)
-    var novaTr = document.createElement("tr") //criando uma tr
-
-    //criando as td
-    var tdNome = document.createElement("td")
-    var tdPeso = document.createElement("td")
-    var tdAltura = document.createElement("td")
-    var tdGordura = document.createElement("td")
-    var tdIMC = document.createElement("td")
-
-    //atribuindo valores ao meus tds
-    tdNome.textContent = nome
-    tdPeso.textContent = peso 
-    tdAltura.textContent = altura
-    tdGordura.textContent = gordura 
-    tdIMC.textContent = calcularIMC(peso,altura)
-
-    //adicionando as td na tr
-    novaTr.appendChild(tdNome)
-    novaTr.appendChild(tdPeso)
-    novaTr.appendChild(tdAltura)
-    novaTr.appendChild(tdGordura)
-    novaTr.appendChild(tdIMC)
-
+    var pacienteTr = criaTr(paciente)
+   
     //adicionando nova liha no tbody
-    tBoy.appendChild(novaTr)
-
-
+    tBoy.appendChild(pacienteTr)
 })
 
 function obtendoDadosDoForm(form){
@@ -43,4 +22,33 @@ function obtendoDadosDoForm(form){
         imc: calcularIMC(form.peso.value, form.altura.value)
     }
     return paciente;
+}
+
+
+function criaTr(paciente){
+
+    var novaTr = document.createElement("tr") //criando uma tr
+
+    //criando as td
+    var tdNome = document.createElement("td")
+    var tdPeso = document.createElement("td")
+    var tdAltura = document.createElement("td")
+    var tdGordura = document.createElement("td")
+    var tdIMC = document.createElement("td")
+
+    //atribuindo valores ao meus tds
+    tdNome.textContent = paciente.nome
+    tdPeso.textContent = paciente.peso 
+    tdAltura.textContent = paciente.altura
+    tdGordura.textContent = paciente.gordura 
+    tdIMC.textContent = paciente.imc
+
+    //adicionando as td na tr
+    novaTr.appendChild(tdNome)
+    novaTr.appendChild(tdPeso)
+    novaTr.appendChild(tdAltura)
+    novaTr.appendChild(tdGordura)
+    novaTr.appendChild(tdIMC)
+
+    return novaTr;
 }
